1f43dbde8fc2386944f097fec8864238c9374b03
Vasiliy Mamaevskiy <toiletpatrol@ya.ru>
2017-01-25 15:53:11 +0300

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,72 @@
+"use strict";
+
+// Meta Number
+function metaNumber(number, raw) {
+  return (typeof raw == 'object') ? calc(raw.number, number, raw.operation) : number;
+}
+
+// Calculation
+function calc(leftOperand, rightOperand, operation) {
+  if (operation == '+') return (leftOperand + rightOperand);
+  if (operation == '-') return (leftOperand - rightOperand);
+  if (operation == '*') return (leftOperand * rightOperand);
+  if (operation == '/') return (leftOperand / rightOperand);
+}
+
+// Numbers
+function zero(raw) {
+  return metaNumber(0, raw);
+}
+
+function one(raw) {
+  return metaNumber(1, raw);
+}
+
+function two(raw) {
+  return metaNumber(2, raw);
+}
+
+function three(raw) {
+  return metaNumber(3, raw);
+}
+
+function four(raw) {
+  return metaNumber(4, raw);
+}
+
+function five(raw) {
+  return metaNumber(5, raw);
+}
+
+function six(raw) {
+  return metaNumber(6, raw);
+}
+
+function seven(raw) {
+  return metaNumber(7, raw);
+}
+
+function eight(raw) {
+  return metaNumber(8, raw);
+}
+
+function nine(raw) {
+  return metaNumber(9, raw);
+}
+
+// Operations
+function plus(number) {
+  return {number, operation: '+'};
+}
+
+function minus(number) {
+  return {number, operation: '-'};
+}
+
+function times(number) {
+  return {number, operation: '*'};
+}
+
+function dividedBy(number) {
+  return {number, operation: '/'};
+}
