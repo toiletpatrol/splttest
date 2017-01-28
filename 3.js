5d9dbae51aa1d0e6ed0c6435075dbfb945e534cd
toiletpatrol <toiletpatrol@ya.ru>
2017-01-29 00:44:12 +0300

    task 3 + tests

---
+++ 3.js
@@ -0,0 +1,44 @@
+"use strict";
+
+// from https://davidwalsh.name/javascript-arguments
+function getFunctionArgs (func) {
+  var args = func.toString().match(/function\s.*?\(([^)]*)\)/)[1];
+ 
+  var a = args.split(',').map(function(arg) {
+    return arg.replace(/\/\*.*\*\//, '').trim().replace(/=.*/, '');
+  }).filter(function(arg) {
+    return arg;
+  });
+
+
+  return a;
+};
+
+// from http://stackoverflow.com/questions/3179861/javascript-get-function-body
+function getFunctionBody(func) {
+  var entire = func.toString();
+  return entire.toString().substring(entire.indexOf("{") + 1, entire.lastIndexOf("}"));
+}
+
+function appendDefaults (args, defaultArguments) {
+  for (var i = 0; i < args.length; i++) {
+    if (defaultArguments[args[i]]) {
+      args[i] += `=${defaultArguments[args[i]]}`;
+    }
+  }
+
+  return args;
+}
+
+module.exports = {
+
+  defaultArguments: function(func, defaultArguments) {
+    var args = getFunctionArgs(func);
+    var body = getFunctionBody(func);
+    
+    args = appendDefaults(args, defaultArguments);
+
+    return new Function(args.join(','), body);
+  }
+
+};
--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var sum = require('../sum');
 
 var assert = require('assert');
 
-
+/*
 describe('Base 2 to base 10', function() {
   it('should convert "10" to 2', function() {
     assert.equal(sum.toDecimal("10"), 2);
@@ -14,7 +14,7 @@ describe('Binary addition', function() {
     assert.equal(sum.sum("101", "10"), 7);
   })
 });
-/*
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
@@ -22,4 +22,49 @@ describe('Array', function() {
     });
   });
 });
+
+
+// 3.js
+var defaultArguments = require('../3')['defaultArguments'];
+
+var add = function (a, b) {
+  return a + b;
+};
+
+var add_;
+
+describe('defaultArguments = { b: 9 }', function() {
+
+  it('add_(10) returns 19', function() {
+    add_ = defaultArguments(add, { b: 9 });
+    assert.equal(add_(10), 19);
+  });
+
+  it('add_(10, 7) returns 17', function() {
+    add_ = defaultArguments(add, { b: 9 });
+    assert.equal(add_(10, 7), 17);
+  });
+
+  it('add_() returns NaN', function() {
+    add_ = defaultArguments(add, { b: 9 });
+    assert.equal(isNaN(add_()), true);
+  });
+
+});
+
+describe('defaultArguments = { b: 3, a: 2 }', function() {
+
+  it('add_(10) returns 13', function() {
+    add_ = defaultArguments(add_, { b: 3, a: 2 });
+    assert.equal(add_(10), 13);
+  });
+  
+  it('add_() returns 5', function() {
+    add_ = defaultArguments(add_, { b: 3, a: 2 });
+    assert.equal(add_(), 5);
+  });
+
+});
 */
+
+// 4.js
